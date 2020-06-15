b5953d7adc2b9659f09009415a049c0d587461f4
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2020-06-14 21:56:17 -0700

    Add some more example code showing off the KotlinEventBus and Forge.kt

--- src/main/kotlin/example/examplemod/ExampleMod.kt
+++ src/main/kotlin/example/examplemod/ExampleMod.kt
@@ -1,9 +1,16 @@
 package example.examplemod
 
+import net.minecraft.block.Block
+import net.minecraftforge.api.distmarker.Dist
+import net.minecraftforge.event.RegistryEvent
 import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent
 import org.apache.logging.log4j.Level
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
+import thedarkcolour.kotlinforforge.forge.FORGE_BUS
+import thedarkcolour.kotlinforforge.forge.MOD_BUS
+import thedarkcolour.kotlinforforge.forge.runWhenOn
 
 @Mod(ExampleMod.ID)
 object ExampleMod {
@@ -12,5 +19,20 @@ object ExampleMod {
 
     init {
         LOGGER.log(Level.INFO, "Hello world!")
+
+        // usage of the KotlinEventBus
+        MOD_BUS.addGenericListener(::onBlockRegistry)
+        FORGE_BUS.addListener(::onServerAboutToStart)
+    }
+
+    private fun onBlockRegistry(event: RegistryEvent.Register<Block>) {
+        // from Forge.kt
+        runWhenOn(Dist.CLIENT) {
+            LOGGER.log(Level.INFO, "Hello client side! I'm in block registry!")
+        }
+    }
+
+    private fun onServerAboutToStart(event: FMLServerAboutToStartEvent) {
+        LOGGER.log(Level.INFO, "Server starting...")
     }
 }
