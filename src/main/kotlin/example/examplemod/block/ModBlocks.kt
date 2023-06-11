8214bd6fd4aef7dcdd1bd49a56c617626d7fba15
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2023-06-11 14:12:47 -0700

    Kotlin for Forge 4.3.0 and use new features from Forge's MDK template

--- src/main/kotlin/example/examplemod/block/ModBlocks.kt
+++ src/main/kotlin/example/examplemod/block/ModBlocks.kt
@@ -3,7 +3,6 @@ package example.examplemod.block
 import example.examplemod.ExampleMod
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.state.BlockBehaviour
-import net.minecraft.world.level.material.Material
 import net.minecraftforge.registries.DeferredRegister
 import net.minecraftforge.registries.ForgeRegistries
 import thedarkcolour.kotlinforforge.forge.registerObject
@@ -14,6 +13,6 @@ object ModBlocks {
     // the returned ObjectHolderDelegate can be used as a property delegate
     // this is automatically registered by the deferred registry at the correct times
     val EXAMPLE_BLOCK by REGISTRY.registerObject("example_block") {
-        Block(BlockBehaviour.Properties.of(Material.BAMBOO).lightLevel { 15 }.strength(3.0f))
+        Block(BlockBehaviour.Properties.of().lightLevel { 15 }.strength(3.0f))
     }
 }
